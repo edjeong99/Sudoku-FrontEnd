dec3ec67ced799f678f170a8b86251ba000d3071
Edward J <edjeong99@gmail.com>
2024-07-22 15:03:13 -0500

    change how alltime is fetched. now it only fetched when game start and difficulty is changed

--- src/components/DisplaySudokuBoard.js
+++ src/components/DisplaySudokuBoard.js
@@ -34,7 +34,7 @@ const DisplaySudokuBoard = ({ puzzle, userInput, hintCells, selectedCell, incorr
       :
       (
         
-          <div  className="grid grid-cols-9 gap-0 border-2 border-gray-800">
+          <div  className="font-cursive grid grid-cols-9 border-2 border-gray-800 overflow-hidden">
          { puzzle.map((row, rowIndex) =>
           row.map((cell, colIndex) => {
             const isIncorrect = incorrectCells.some(
@@ -44,12 +44,13 @@ const DisplaySudokuBoard = ({ puzzle, userInput, hintCells, selectedCell, incorr
               <div
                 key={`${rowIndex}-${colIndex}`}
                 className={`
-                  w-10 h-10 text-center outline-none
-                  border border-gray-300 font-bold
-                  ${rowIndex % 3 === 0 ? "border-t-2 border-t-gray-800" : ""}
-                  ${colIndex % 3 === 0 ? "border-l-2 border-l-gray-800" : ""}
-                  ${rowIndex === 8 ? "border-b-2 border-b-gray-800" : ""}
-                  ${colIndex === 8 ? "border-r-2 border-r-gray-800" : ""}
+                   relative
+              before:content-[''] before:float-left before:pb-[100%]
+              border-r border-b border-gray-300 font-cursive font-bold
+               ${(rowIndex + 1) % 3 === 0 ? 'border-b-2 border-b-gray-800' : ''}
+              ${(colIndex + 1) % 3 === 0 ? 'border-r-2 border-r-gray-800' : ''}
+              ${rowIndex === 0 ? 'border-t border-t-gray-300' : ''}
+              ${colIndex === 0 ? 'border-l border-l-gray-300' : ''}
                   ${ // decide background color - cliecked, wrong, hint, etc
                     cell !== 0
                       ? " bg-gray-100"
@@ -75,17 +76,21 @@ const DisplaySudokuBoard = ({ puzzle, userInput, hintCells, selectedCell, incorr
                 `}
                 onClick={() => setSelectedCell({ rowIndex, colIndex })}
               >
-               {isMobile ? (
-                  userInput[rowIndex][colIndex] || (cell !== 0 ? cell : '')
-                ) : (
-                  <input
-                    type="text"
-                    value={userInput[rowIndex][colIndex]}
-                    onChange={(e) => handleInputChange(e, rowIndex, colIndex)}
-                    disabled={cell !== 0}
-                    className="w-full h-full text-center outline-none bg-transparent"
-                  />
-                )}
+               <div className="absolute inset-0 flex items-center justify-center font-cursive">
+              {isMobile ? (
+                <span className="text-lg font-bold font-cursive">
+                  {userInput[rowIndex][colIndex] || (cell !== 0 ? cell : '')}
+                </span>
+              ) : (
+                <input
+                  type="text"
+                  value={userInput[rowIndex][colIndex] || (cell !== 0 ? cell : '')}
+                  className="w-full h-full text-center outline-none bg-transparent text-lg font-bold font-cursive"
+                  readOnly={cell !== 0}
+                  onChange={(e) => handleInputChange(e, rowIndex, colIndex)}
+                />
+              )}
+            </div>
               </div>
             );
           })
--- src/components/SudokuBoard.js
+++ src/components/SudokuBoard.js
@@ -19,18 +19,69 @@ const SudokuBoard = ({ difficulty }) => {
   const [resetTimer, setResetTimer] = useState(false); // boolean to reset timer
   const [incorrectCells, setIncorrectCells] = useState([]); // array of incorrect cells when 'check' button is cliced
   const [selectedCell, setSelectedCell] = useState(null);
-const[beginTime, setBeginTime] = useState(0);
-const [chartData, setChartData] = useState({ allTimes: [], playerTime: null });
-
+  const [beginTime, setBeginTime] = useState(0);
+  const [chartData, setChartData] = useState({
+    allTimes: [],
+    playerTime: null,
+  });
+  const [allTimes, setAllTimes] = useState([])
 
   const API_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
-  console.log("API_URL is ",API_URL )
-
- 
+  console.log("API_URL is ", API_URL);
 
   useEffect(() => {
     fetchPuzzle();
+    getAllTimes();
   }, [difficulty]);
+
+  const getAllTimes = () =>{
+    const replaceZeroWithPrevious = (data) => {
+      const replacedData = [...data];
+      let lastNonZero = null;
+      for (let i = 0; i < data.length; i++) {
+        if (data[i] === 0) {
+          replacedData[i] = lastNonZero !== null ? lastNonZero : data[i];
+        } else {
+          lastNonZero = data[i];
+        }
+      }
+      return replacedData;
+    };
+  
+ 
+        
+        // Find the max completion time
+        const maxTime = Math.max(...allTimes);
+        
+        // Create an array from 0 to maxTime
+        const timeRange = Array.from({ length: maxTime + 1 }, (_, i) => i);
+            // Count frequency of each completion time
+            const timeFrequency = allTimes.reduce((acc, time) => {
+              acc[time] = (acc[time] || 0) + 1;
+              return acc;
+            }, {});
+            
+      const allPlayersData = replaceZeroWithPrevious(timeRange.map(time => timeFrequency[time] || 0));
+  
+     axios
+      .get(`${API_URL}/getAllTimes`)
+      .then((response) => {
+        let times = (response.data.allTimes);
+        const maxTime = Math.max(...times);
+        const timeRange = Array.from({ length: maxTime + 1 }, (_, i) => i);
+        // Count frequency of each completion time
+        const timeFrequency = times.reduce((acc, time) => {
+          acc[time] = (acc[time] || 0) + 1;
+          return acc;
+        }, {});
+          const allPlayersData = replaceZeroWithPrevious(timeRange.map(time => timeFrequency[time] || 0));
+          setAllTimes(allPlayersData)
+        console.log(allPlayersData)
+        })
+      .catch((error) => {
+        console.error("There was an error fetching the allTImes!", error);
+      });
+  }
 
   const fetchPuzzle = () => {
     axios
@@ -38,39 +89,41 @@ const [chartData, setChartData] = useState({ allTimes: [], playerTime: null });
       .then((response) => {
         let emptyCell = 0;
         console.log(response.data);
-          const { puzzle, solution } = response.data;
-          const initialUserInput = puzzle.map((row) => // init userInput and count number of empty cell for display stat 
-            row.map((cell) =>{
-              if(cell !== 0)
-                return cell
-              else
-              emptyCell++;
-            return ""
-          })); 
-          
+        const { puzzle, solution } = response.data;
+        const initialUserInput = puzzle.map(
+          (
+            row // init userInput and count number of empty cell for display stat
+          ) =>
+            row.map((cell) => {
+              if (cell !== 0) return cell;
+              else emptyCell++;
+              return "";
+            })
+        );
+
         setPuzzle(puzzle);
         setSolution(solution);
         setUserInput(initialUserInput);
         setMessage("");
         setHintCells(new Set()); // Reset hinted cell
-        setIsSolved(false); 
+        setIsSolved(false);
         setResetTimer(!resetTimer); // for timer reset
         setStat("Correct : 0     Wrong : 0    Empty : " + `${emptyCell}`);
         setBeginTime(Date.now());
-        console.log(beginTime)
+        console.log(beginTime);
       })
       .catch((error) => {
         console.error("There was an error fetching the puzzle!", error);
       });
   };
 
- 
   const handleNumberSelect = (number) => {
     if (selectedCell) {
       const { rowIndex, colIndex } = selectedCell;
-      let prevVal = userInput[rowIndex][colIndex]
-      let newVal = number === 'x' ? prevVal.slice(0,-1) : prevVal + number.toString()
-      handleInputChange({ target: { value:  newVal } }, rowIndex, colIndex);
+      let prevVal = userInput[rowIndex][colIndex];
+      let newVal =
+        number === "x" ? prevVal.slice(0, -1) : prevVal + number.toString();
+      handleInputChange({ target: { value: newVal } }, rowIndex, colIndex);
       //setSelectedCell(null);
     }
   };
@@ -80,45 +133,48 @@ const [chartData, setChartData] = useState({ allTimes: [], playerTime: null });
     const newBoard = userInput.map((r, rowIndex) =>
       r.map((cell, colIndex) =>
         rowIndex === row && colIndex === col
-          ? (value !== "" ? value : "")
+          ? value !== ""
+            ? value
+            : ""
           : cell
       )
     );
     setUserInput(newBoard);
   };
 
-/*  
+  /*  
 saves users game time.  retrieve all completed times for this difficulty
 */
-const handleGameCompleted = (correctCount) => {
-  // This function is called when the game is solved
-  setMessage(`COMPLETED!!!  ${correctCount} cells solved.`);
-  setIsSolved(true);
-  
-  let userId = localStorage.getItem('userId');
- // console.log(userId);
-  if(!userId) return;
-  let playTime = Math.floor((Date.now() - beginTime)/1000);
+  const handleGameCompleted = (correctCount) => {
+    // This function is called when the game is solved
+    setMessage(`COMPLETED!!!  ${correctCount} cells solved.`);
+    setIsSolved(true);
+    let playTime = Math.floor((Date.now() - beginTime) / 1000);
+    const duration = Math.floor(playTime); // Time in seconds
+    setChartData({ allTimes: allTimes, playerTime: playTime });
 
-  const duration = Math.floor(playTime); // Time in seconds
-
-
- axios
- .post(`${API_URL}/user/saveSudokuTime`, { time: duration, difficulty:difficulty }, {
-      headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
-    })
-  .then((response) => {
-       const { allTimes, message } = response.data;
-    console.log(message);
-console.log(allTimes );
-
-setChartData({allTimes:allTimes, playerTime:playTime})
-})
-  .catch ((error) => {
-    console.error('Error saving Sudoku time:', error);
-  
-});
-};
+    let userId = localStorage.getItem("userId");
+    // console.log(userId);
+    if (!userId) return;
+   
+    axios
+      .post(
+        `${API_URL}/user/saveSudokuTime`,
+        { time: duration, difficulty: difficulty },
+        {
+          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+        }
+      )
+      .then((response) => {
+        const { message } = response.data;
+        console.log(message);
+       
+        
+      })
+      .catch((error) => {
+        console.error("Error saving Sudoku time:", error);
+      });
+  };
 
   const handleCheckClick = () => {
     console.log("handleCheckClick");
@@ -133,9 +189,10 @@ setChartData({allTimes:allTimes, playerTime:playTime})
       row.forEach((_, j) => {
         const userInputValue = userInput[i][j];
         const solutionValue = solution[i][j];
-        const originalValue = puzzle[i][j]; 
+        const originalValue = puzzle[i][j];
 
-        if (originalValue === 0) { // cell was empth and should check if user input is correct 
+        if (originalValue === 0) {
+          // cell was empth and should check if user input is correct
           if (userInputValue === "" || userInputValue.length > 1) {
             emptyCount++;
           } else if (userInputValue !== solutionValue.toString()) {
@@ -147,21 +204,17 @@ setChartData({allTimes:allTimes, playerTime:playTime})
         }
       });
     });
-    
-   
+
     // Construct the stat and message
     setStat(
       `Correct : ${correctCount}     Wrong : ${wrongCount}    Empty : ${emptyCount}`
     );
     setIncorrectCells(incorrectCells);
 
-    // if game is completed 
+    // if game is completed
     if (wrongCount == 0 && emptyCount == 0) {
-      handleGameCompleted(correctCount)
+      handleGameCompleted(correctCount);
     }
-
-
-   
   };
 
   const requestHint = async () => {
@@ -198,7 +251,7 @@ setChartData({allTimes:allTimes, playerTime:playTime})
   };
 
   return (
-    <div className="p-4 w-3/4 max-w-96 min-w-64 mx-auto">
+    <div className="p-2 sm:p-4 w-full max-w-[500px] mx-auto">
       <h1 className="text-3xl font-bold text-center mb-6 text-blue-600">
         Sudoku Game
       </h1>
@@ -206,20 +259,19 @@ setChartData({allTimes:allTimes, playerTime:playTime})
       <div className="text-lg font-semibold mb-4">
         Difficulty : {difficulty}
       </div>
-      <DisplaySudokuBoard 
+      <DisplaySudokuBoard
         puzzle={puzzle}
         userInput={userInput}
         hintCells={hintCells}
         selectedCell={selectedCell}
-        incorrectCells={incorrectCells} 
+        incorrectCells={incorrectCells}
         setSelectedCell={setSelectedCell}
         handleInputChange={handleInputChange}
         handleNumberSelect={handleNumberSelect}
         chartData={chartData}
         isSolved={isSolved}
       />
-     
- 
+
       <BottomComponent
         handleCheckClick={handleCheckClick}
         fetchPuzzle={fetchPuzzle}
